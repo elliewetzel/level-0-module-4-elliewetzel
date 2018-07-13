bdde91b4f70744b8260bfbadcce565403fbfc100
elliewetzel <league@WorthyMini-5.attlocal.net>
2018-07-13 11:55:33 -0700

    Level 0, Module 4.  Change calculator and part of test scores.

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -14,17 +14,21 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+		String nickels = JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int using Integer.parseInt()
-
+		int n = Integer.parseInt(nickels);
 		// Ask the user how many dimes they have, and convert their answer
-
+		String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+		int d = Integer.parseInt(dimes);
 		// Ask the user how many quarters they have, and convert their answer
-
+		String quarters = JOptionPane.showInputDialog("How many quarters do you have?");
+		int q = Integer.parseInt(quarters);
 		// Calculate how much money the user has and save it in a double variable 
-
+		Double e = .05*n;
+		Double k = .10*d;
+		Double l = .25*q;
 		// Tell the user how much money they have
-
+		JOptionPane.showMessageDialog(null, e + k + l);
 	}
 }
 
---
+++ src/TestScores.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class TestScores {
+public static void main(String[] args) {
+	String score = JOptionPane.showInputDialog("What is you test score?");
+	int s = Integer.parseInt(score);
+	
+}
+}
