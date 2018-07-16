329b503b12f5819e99c573beb143efa76f7cc385
elliewetzel <40366109+elliewetzel@users.noreply.github.com>
2018-07-16 12:04:19 -0700

    Add files via upload

--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -9,7 +9,7 @@ public class SleepyHead {
 
     public static void main(String[] args) {
         
-        boolean isWeekday, isVacation;
+        boolean isWeekday = false, isVacation = false;
         
         /* You MUST use the above boolean variables in your code */
         
@@ -17,13 +17,37 @@ public class SleepyHead {
          * Ask the user for these values using a confirm dialog like the one below
          * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
          */
-    
-
-
+        int weekday = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+        System.out.println(weekday);
+        if(weekday == 0) {
+        	isWeekday = true;
+        }
+        else if(weekday == 1) {
+        	isWeekday = false;
+        }
+        int vacation = JOptionPane.showConfirmDialog(null, "Are you on vacation?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+       System.out.println(vacation);
+        if(vacation == 0) {
+        	isVacation = true;
+        }
+        else if(vacation == 1) {
+        	isVacation = false;
+        }
+        if(isVacation) {
+        	JOptionPane.showMessageDialog(null, "sleep in");
+        }
+        else if(isWeekday) {
+        	JOptionPane.showMessageDialog(null, "get up lazybones!");
+        } if(isWeekday) {
+        	JOptionPane.showMessageDialog(null, "get up lazybones!");
+        
+    }
+    }
+}
         /*
          * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
          * print “get up lazybones!” If it is a weekday, and we are on vacation,
          * print “sleep in”.
          */
-    }
-}
+    
+
--- src/StringMethods1.java
+++ src/StringMethods1.java
@@ -7,23 +7,29 @@ public class StringMethods1 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
+		String e = "elephant";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		System.out.println(e.charAt(2));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length
-
+		System.out.println(e.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
-		
+		for(int i = 0; i < e.length(); i++) {
+			System.out.println(e.charAt(i));
+		}
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+		char j = 'a';
+		for(int i = 0; i < e.length(); i++) {
+			if(j==e.charAt(i)) {
+				System.out.println(i);
+			}
+		}
 	}
 }
 
